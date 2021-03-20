5bd271225705c7cb532b8f2e89fad19a88dcf268
Angel Mgl <angelemegeele@gmail.com>
2021-03-20 15:33:18 -0300

    Client Side Generation

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,10 +1,36 @@
+import React from 'react';
+import Link from 'next/link';
 import Layout from '../../components/Layout';
 import Title from '../../components/Title';
 
 export default function Posts() {
+    const [ posts, setPosts] = React.useState([]);
+
+    React.useEffect(() => {
+        const fetchPosts = async () => {
+            const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+            const newPosts = await res.json();
+            setPosts(newPosts);
+        };
+
+        fetchPosts();
+    }, []);
+
     return(
         <Layout>
             <Title>Posts page</Title>
+            <div>
+                {posts.map(post => {
+                    return(
+                        <Link href={`/posts/[id]`} as={`/posts/${post.id}`} key={post.id}>
+                            <a>
+                                <h3>{post.title}</h3>
+                                <p>{post.body}</p>
+                            </a>
+                        </Link>
+                    )
+                })}
+            </div>
         </Layout>
     )
 }
