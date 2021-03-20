f6e62536ad40e22a1d460000f69e2e32f3cb2fb3
Angel Mgl <angelemegeele@gmail.com>
2021-03-20 17:04:39 -0300

    Static Content

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -2,7 +2,7 @@ import { useRouter } from 'next/router';
 import Layout from '../../components/Layout';
 import Title from '../../components/Title';
 
-export default function User() {
+export default function User({ user }) {
     const router = useRouter();
     //console.log(router)
 
@@ -12,4 +12,15 @@ export default function User() {
             <p>User: {router.query.id}</p>
         </Layout>
     )
+}
+
+export async function getStaticProps(context) {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users/' + context.params.id);
+    const user = await res.json();
+
+    return {
+        props: {
+            user
+        }
+    }
 }
