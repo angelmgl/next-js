534571f2246de4eedff8e99e6e406cfe0b58a09c
Angel Mgl <angelemegeele@gmail.com>
2021-03-20 16:45:40 -0300

    Server Side Rendering

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,14 +1,57 @@
-import { useRouter } from 'next/router';
-import Layout from '../../components/Layout';
-import Title from '../../components/Title';
+import Layout from "../../components/Layout";
+import Title from "../../components/Title";
 
-export default function Post() {
-    const router = useRouter();
-
-    return(
+export default function Post({ post }) {
+    return (
         <Layout>
             <Title>Post details</Title>
-            <p>Post: {router.query.id}</p>
+            <div className="card">
+                <h2>{post.title}</h2>
+                <p>{post.body}</p>
+            </div>
+            <style jsx>
+                {`
+                    .card {
+                        margin: 1rem;
+                        flex-basis: 45%;
+                        padding: 1.5rem;
+                        text-align: left;
+                        color: black;
+                        text-decoration: none;
+                        border: 2px solid #eaeaea;
+                        border-radius: 10px;
+                        transition: color 0.15s ease, border-color 0.15s ease;
+                    }
+                    .card:hover,
+                    .card:focus,
+                    .card:active {
+                        color: #0070f3;
+                        border-color: #0070f3;
+                    }
+                    .card h3 {
+                        margin: 0 0 1rem 0;
+                        font-size: 1.5rem;
+                    }
+                    .card p {
+                        margin: 0;
+                        font-size: 1.25rem;
+                        line-height: 1.5;
+                    }
+                `}
+            </style>
         </Layout>
-    )
+    );
+}
+
+export async function getServerSideProps(context) {
+    const res = await fetch(
+        "https://jsonplaceholder.typicode.com/posts/" + context.params.id
+    );
+    const post = await res.json();
+
+    return {
+        props: {
+            post,
+        },
+    };
 }
