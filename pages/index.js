c6dcac4dca2d58c04137b62518f8d915c65dab13
Angel Mgl <angelemegeele@gmail.com>
2021-03-20 12:37:40 -0300

    Initial setup

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return(
+        <div>
+            <h1>Welcome to the Home page!</h1>
+            <p>This is my first webpage made in Next js.</p>
+        </div>
+    )
+}
